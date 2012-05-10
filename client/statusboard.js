9988c702669d75c7c2757de9cee974e7745205b0
Andrew Beresford <beezly@beezly.org.uk>
2012-05-10 10:10:45 +0100

    Remove unnecessary statemap function from client side Add all entries from VShell JSON on server side

--- client/statusboard.js
+++ client/statusboard.js
@@ -1,14 +1,3 @@
-
-
 Template.services.services = function () {
   return Services.find({host: "Web Tests"}, {sort: {service: 1}});
-}
-
-function getState(state) {
-	var statemap = []
-	statemap[0] = "OK";
-	statemap[1] = "WARNING";
-	statemap[2] = "CRITICAL";
-	statemap[3] = "UNKNOWN";
-	return statemap[state];
 }
--- server/statusboard.js
+++ server/statusboard.js
@@ -1,3 +1,5 @@
+var SERVICES_URL='http://status.shef.ac.uk/vshell/index.php?type=services&mode=json'
+
 var statemap = [];
 statemap[0] = "OK";
 statemap[1] = "WARNING";
@@ -5,11 +7,11 @@ statemap[2] = "CRITICAL";
 statemap[3] = "UNKNOWN";
 
 function getServices() {
-  Meteor.http.call('GET', 'http://status.shef.ac.uk/vshell/index.php?type=services&mode=json', function (error, result) {
+  Meteor.http.call('GET', SERVICES_URL, function (error, result) {
     var resJSON = result.data;
     _.each(resJSON, function(data) {
       var host = data["host_name"];
-      if (host == 'Web Tests') {
+//      if (host == 'Web Tests') {
         var service = data["service_description"];
         var hardState = statemap[data["last_hard_state"]];
         var currState = statemap[data["current_state"]];
@@ -24,7 +26,7 @@ function getServices() {
             Services.update({host: host, service: service}, {host: host, service: service, hardState: hardState, currState: currState},function(){});
           }
         }
-      }
+//      } // if(host == 'Web Tests)
     });
   });
 } 
